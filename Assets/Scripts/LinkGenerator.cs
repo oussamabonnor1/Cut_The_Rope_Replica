078e009e6ab6e52afba426142bdbc7928ac885dc
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-24 00:24:38 +0100

    adding multiple chain generation for a signle weight

--- Assets/Scripts/LinkGenerator.cs
+++ Assets/Scripts/LinkGenerator.cs
@@ -6,6 +6,7 @@ public class LinkGenerator : MonoBehaviour
 {
     public Rigidbody2D rb, previousBody;
     public GameObject linkPrefab;
+    public GameObject weight;
     public int chainLength;
 
     // Start is called before the first frame update
@@ -18,5 +19,13 @@ public class LinkGenerator : MonoBehaviour
             link.GetComponent<HingeJoint2D>().connectedBody = previousBody;
             previousBody = link.GetComponent<Rigidbody2D>();
         }
+        linkWeight(previousBody);
+    }
+
+    void linkWeight(Rigidbody2D previousBody){
+        HingeJoint2D weightJoint = weight.AddComponent<HingeJoint2D>();
+        weightJoint.autoConfigureConnectedAnchor = false;
+        weightJoint.connectedBody = previousBody;
+        weightJoint.connectedAnchor = new Vector2(0,-.3f);
     }
 }
