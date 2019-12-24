a035d47c41835fee75aedbd1d5ae9b19537c44ac
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-24 12:03:28 +0100

    adding line effect for cuts

---
+++ Assets/Scripts/LineBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LineBehaviour : MonoBehaviour {
+
+	public LineRenderer line;
+	public EdgeCollider2D lineCollider;
+	public List<Vector2> points;
+
+	public void updateLine (Vector2 mousePosition) {
+
+		if (points == null) {
+			points = new List<Vector2>();
+			setPoint(mousePosition);
+			return;
+		}
+		if (Vector2.Distance (points.Last (), mousePosition) >.1f) {
+			setPoint(mousePosition);
+		}
+	}
+
+	void setPoint (Vector2 mousePosition) {
+		points.Add (mousePosition);
+		line.positionCount = points.Count;
+		line.SetPosition (points.Count - 1, mousePosition);
+		if (points.Count > 1) lineCollider.points = points.ToArray ();
+	}
+}
---
+++ Assets/Scripts/LineManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LineManager : MonoBehaviour {
+
+	public GameObject linePrefab;
+	public LineBehaviour activeLine;
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject line = Instantiate (linePrefab);
+			activeLine = line.GetComponent<LineBehaviour> ();
+		}
+
+		if (activeLine != null) {
+			Vector2 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			activeLine.updateLine (mousePosition);
+		}
+
+		if (Input.GetMouseButtonUp (0)){ 
+			activeLine = null;
+		}
+	}
+
+
+	public void restartScene(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
